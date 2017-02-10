fbbea1fca5635cc79d04b27f435a316fcedf20fd
xiaolu.zhang <xiaolu.zhang@weimob.com>
2017-02-10 19:39:09 +0800

    更新未完成

--- src/main/java/com/taylor/action/TestController.java
+++ src/main/java/com/taylor/action/TestController.java
@@ -12,20 +12,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taylor.entity.Test;
 import com.taylor.service.TestService;
+import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
+@Api(value = "/test", description = "H5购物车接口")
 @RequestMapping("/test")
 @Controller
 public class TestController extends BaseAction {
 
-    @Autowired
-    private TestService testService;
+	@Autowired
+	private TestService testService;
 
-    @ResponseBody
-    @RequestMapping("/query")
-    @ApiOperation(value = "测试", httpMethod = "POST", response = List.class, notes = "query test")
-    public List<Test> queryTest(Test test, HttpServletRequest request, HttpServletResponse response) {
-        log.debug("这只是一个测试");
-        return testService.queryTest(test);
-    }
+	@ResponseBody
+	@RequestMapping("/query")
+	@ApiOperation(value = "测试", httpMethod = "POST", response = List.class, notes = "query test")
+	public List<Test> queryTest(Test test, HttpServletRequest request, HttpServletResponse response) {
+		log.debug("这只是一个测试");
+		return testService.queryTest(test);
+	}
 }
--- src/main/java/com/taylor/config/SwaggerConfig.java
+++ src/main/java/com/taylor/config/SwaggerConfig.java
@@ -3,42 +3,60 @@ package com.taylor.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
 import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.paths.SwaggerPathProvider;
 import com.mangofactory.swagger.plugin.EnableSwagger;
 import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
 
-/**
- * 项目名称：apidoc
- * 
- * @description:
- * @author Wind-spg
- * @create_time：2015年2月10日 上午10:27:51
- * @version V1.0.0
- */
 @Configuration
 @EnableSwagger
-public class SwaggerConfig {
-    private SpringSwaggerConfig springSwaggerConfig;
-    /**
-     * Required to autowire SpringSwaggerConfig
-     */
-    @Autowired
-    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
-        this.springSwaggerConfig = springSwaggerConfig;
-    }
-    /**
-     * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc framework - allowing for
-     * multiple swagger groups i.e. same code base multiple swagger resource listings.
-     */
-    @Bean
-    public SwaggerSpringMvcPlugin customImplementation() {
-        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo()).includePatterns(".*?");
-    }
+@EnableWebMvc
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
-    private ApiInfo apiInfo() {
-        ApiInfo apiInfo = new ApiInfo("swagger", "swagger-test", "My Apps API terms of service", "516195940@qq.com", "taylor", "taylor.com");
-        return apiInfo;
-    }
+	private SpringSwaggerConfig springSwaggerConfig;
+
+	@Autowired
+	public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+		this.springSwaggerConfig = springSwaggerConfig;
+	}
+
+	/**
+	 * 链式编程 来定制API样式 后续会加上分组信息
+	 * 
+	 * @return
+	 */
+	@Bean
+	public SwaggerSpringMvcPlugin customImplementation() {
+		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo()).includePatterns(".*?");
+
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo("API接口平台",null, null, null,null, null);
+		return apiInfo;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	class GtPaths extends SwaggerPathProvider {
+
+		@Override
+		protected String applicationPath() {
+			return "/restapi";
+		}
+
+		@Override
+		protected String getDocumentationPath() {
+			return "/restapi";
+		}
+	}
+
 }
