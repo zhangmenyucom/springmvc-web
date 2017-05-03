240eb779bbac188c601d7327cca0a47f1c9b155e
zhangmenyucom <zhangmenyucom@github.com>
2017-05-04 07:36:58 +0800

    更新

--- src/main/java/com/taylor/app/Constants.java
+++ src/main/java/com/taylor/app/Constants.java
@@ -2,8 +2,11 @@ package com.taylor.app;
 
 public class Constants {
 	/**cookie信息**/
-	public static final String cookie = "aliyungf_tc=AQAAAIdOdwfd5QgAb9hAcCP6PrFonOA9; SESSION=a7572b2f-3df7-4159-9c04-1c08627fb6e5";
+	public static final String cookie = "aliyungf_tc=AQAAAItpvWph/QQAb9hAcG0qzqxQGjtt; SESSION=d1382515-8af1-40b5-8956-e32d307cb1c7";
 	
 	/**加载因子**/
-	public static final int factor = new Integer(1);
+	public static final int factor = new Integer(4);
+	
+	/**最大倍数**/
+	public static final int maxMutiply=new Integer(5800);
 }
--- src/main/java/com/taylor/app/SigaporeAppQianShiWei.java
+++ src/main/java/com/taylor/app/SigaporeAppQianShiWei.java
@@ -41,15 +41,21 @@ public class SigaporeAppQianShiWei {
 						System.out.println("方案：大小单双前二星压十位---最新一期号码：" + gameOpenCode.getIssue() + "开奖码：" + gameOpenCode.getCode() + ",尾数为：------" + cureentNum + "--------" + (cureentNum % 2 == 0 ? "双" : "单"));
 						if (cureentNum % 2 == lastNum % 2 && lastNum != -1) {
 							initMutiply = factor;
-							System.out.println("恭喜你中奖了。。。");
+                            System.out.println("++++++++++++++++++++++恭喜你中奖了++++++++++++++++++++++++++");
+                            System.out.println("");
 						} else {
 							if (lastNum != -1) {
 								initMutiply = 2 * initMutiply + factor;
 							}
 						}
 						lastNum = cureentNum;
+						/**收手重来**/
+                        if (initMutiply > Constants.maxMutiply) {
+                            initMutiply = Constants.maxMutiply / 4;
+                        }
+                        System.out.println("当前金额：【----------------"+resp.getData().getLotteryBalance()+"----------------】元");
 						singapore30OrderPost.postOrder("dxdsq",initMutiply, lastNum % 2 == 0 ? "双,单双" : "单,单双");
-						System.out.println("加注成功，加注倍数:"+initMutiply+"下注方案：("+(lastNum % 2 == 0 ? "双,单双" : "单,单双")+"),操作时间:"+new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(new Date()));
+						System.out.println("加注成功，加注倍数:【"+initMutiply+"】,下注方案：【"+( lastNum % 2 == 0 ? "双,单双" : "单,单双")+"】,操作时间:【"+new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(new Date())+"】");
 					}
 				} else {
 					System.out.println("执行结果出错。。。");
