9f0b1a14b3107998f85fd0e6ec7bf989a66f8ed5
zhangmenyucom <zhangmenyucom@github.com>
2017-05-08 07:08:46 +0800

    dcsc se

---
+++ src/main/java/com/taylor/app/inverse/MainApp.java
@@ -0,0 +1,10 @@
+package com.taylor.app.inverse;
+
+public class MainApp {
+	public static void main(String[] args) {
+		new SigaporeAppHouGeWei().start();
+		new SigaporeAppHouShiWei().start();
+		new SigaporeAppQianGeWei().start();
+		new SigaporeAppQianShiWei().start();
+	}
+}
