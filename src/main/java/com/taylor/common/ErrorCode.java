ed85913cb9f81af3a52ae046e450e73ba3aeabf3
xiaolu.zhang <516195940@qq.com>
2017-07-10 16:35:29 +0800

    更新

--- src/main/java/com/taylor/common/ErrorCode.java
+++ src/main/java/com/taylor/common/ErrorCode.java
@@ -2,12 +2,12 @@ package com.taylor.common;
 
 public interface ErrorCode {
 
-    public static final int SUCCESS = 0;
-    public static final int FAILED = 40001;
-    public static final int ORDER_DISEXIST = 40002;
-    public static final int PAYSTATUS_ERROR = 40003;
+    int SUCCESS = 0;
+    int FAILED = 40001;
+    int ORDER_DISEXIST = 40002;
+    int PAYSTATUS_ERROR = 40003;
 
-    public String getMsg();
+    String getMsg();
 
     int getCode();
 
