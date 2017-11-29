3e1ca2150e6823b86d14d3903274f82d81e63856
xiaolu.zhang <516195940@qq.com>
2017-11-29 18:15:14 +0800

    更新

--- src/main/java/com/taylor/action/TestController.java
+++ src/main/java/com/taylor/action/TestController.java
@@ -1,8 +1,10 @@
 package com.taylor.action;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.taylor.dto.TestBean;
+import com.taylor.entity.Test;
+import com.taylor.service.TestService;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.taylor.dto.TestBean;
-import com.taylor.entity.Test;
-import com.taylor.service.TestService;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Api(value = "/test", description = "H5购物车接口")
 @RequestMapping("/test")
@@ -32,5 +32,12 @@ public class TestController extends BaseAction {
         BeanUtils.copyProperties(testBean, test);
         log.debug("这只是一个测试");
         return testService.queryTest(test).get(0);
+    }
+
+    @ResponseBody
+    @RequestMapping("/test")
+    @ApiOperation(value = "获取购物车", httpMethod = "POST", response = String.class, notes = "test")
+    public String test(@RequestBody List<Integer> list, HttpServletRequest request, HttpServletResponse response) {
+        return list.toString();
     }
 }
